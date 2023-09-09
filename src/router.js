1703b592ca873e36a8584d27fd8e21337a08a4dd
Alex A. Cita <alex_cita@hotmail.com>
2023-09-09 15:50:18 -0300

    feat: add validate from middleware

--- src/router.js
+++ src/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const taskController = require('./controllers/taskController')
+const taskMiddleware = require('./middleware/taksMiddleware')
 
 const router = express.Router();
 
 router.get('/health', (req, res) => res.status(200).json({ status: 'ok'}));
 router.get('/tasks', taskController.getAll);
-router.post('/tasks', taskController.createTask);
+router.post('/tasks', taskMiddleware.validateBody, taskController.createTask);
 
 module.exports = router;
